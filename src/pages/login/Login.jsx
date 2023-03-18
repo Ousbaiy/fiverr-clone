1e07df7d8a741672f2d33b1bf7aec2f8a03d1261
Oussama Es-Sebaiy <84465604+Ousbaiy@users.noreply.github.com>
2023-03-18 14:26:15 +0300

    fixed login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './login.scss'
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <form>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="John Deo"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Continue</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login
